44ae151443773841c6c5cc13876aa77eb54a81f3
scchn <scxchn@gmail.com>
2023-11-14 20:41:07 +0800

    Update documentation

--- Sources/ZPLBuilder/Commands/Hyphen.swift
+++ Sources/ZPLBuilder/Commands/Hyphen.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 public enum Hyphen: ZPLCommandConvertible {
-    /// \&. Carriage return/line feed
+    /// \\&. Carriage return/line feed
     case `return`
-    /// \(*). Soft hyphen (word break with a dash)
+    /// \\(*). Soft hyphen (word break with a dash)
     case softHyphen(Character)
-    /// \\. = backslash (\)
+    /// \\. = backslash (\\)
     ///
-    /// ^CI13 must be selected to print a backslash (\).
+    /// ^CI13 must be selected to print a backslash (\\).
     case backslash
     
     public var command: String {
